b887e6621f7d3684df0a45ce12d46ad4910d02f7
Pavan Ch <pk7057764@gmail.com>
2025-11-15 12:31:54 +0530

    file are added in face

---
+++ python-codes/imageprocessing/opencv-face-detect.py
@@ -0,0 +1,12 @@
+import cv2
+
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+img = cv2.imread("people.jpg")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.2, 4)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+cv2.imwrite("faces_detected.jpg", img)
+
